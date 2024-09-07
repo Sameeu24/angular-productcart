1af62b085a7a452af6178e5b92234017db7e4ccb
Abdul Sameeu <abdulsameeu26@gmail.com>
2024-09-07 16:25:21 +0530

    Update data

--- src/app/foodmenu/foodmenu.component.ts
+++ src/app/foodmenu/foodmenu.component.ts
@@ -11,33 +11,39 @@ import { FoodGalleryComponent } from '../foodgallery/foodgallery.component'
 export class FoodMenuComponent {
   foodItems = [
     {
-      url: 'https://example.com/image1.jg',
-      title: 'Image 1',
-      subtitle: 'This is the first image',
-      price:'23'
+      url: "/images/image-waffle-desktop.jpg",
+      title: "Waffle with Berries",
+      subtitle: "Waffle",
+      price:"23"
     },
     {
-      url: 'https://example.com/image1.jpg',
-      title: 'Image 2',
-      subtitle: 'This is the second image',
+      url: "/images/image-creme-brulee-desktop.jpg",
+      title: "Vanilla Bean Crème Brûlée",
+      subtitle: "Crème Brûlée",
+      price:"7.00"
+    },
+    {
+      url: "/images/image-macaron-desktop.jpg",
+      title:"Macaron Mix of Five",
+      subtitle:"Macaron",
+      price:"8.00"
+    },
+    {
+      url: "/images/image-tiramisu-desktop.jpg",
+      title:"Classic Tiramisu",
+      subtitle: "Tiramisu",
+      price:"5.50"
+    },
+    {
+      url: "/images/image-baklava-desktop.jpg",
+      title: "Pistachio Baklava",
+      subtitle: "Baklava",
       price:'24'
     },
     {
-      url: 'https://example.com/image1.jpg',
-      title: 'Image 2',
-      subtitle: 'This is the second image',
-      price:'24'
-    },
-    {
-      url: 'https://example.com/image1.jpg',
-      title: 'Image 2',
-      subtitle: 'This is the second image',
-      price:'24'
-    },
-    {
-      url: 'https://example.com/image1.jpg',
-      title: 'Image 2',
-      subtitle: 'This is the second image',
+      url: "/images/image-meringue-desktop.jpg",
+      title:  "Lemon Meringue Pie",
+      subtitle: "Pie",
       price:'24'
     }
     // Add more image objects as needed
